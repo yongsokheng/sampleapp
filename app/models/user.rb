c540f8009e8a780ea1f6816b55dd669d14bcb6da
yongsokheng <your.yongsokheng@ymail.com>
2015-04-15 15:21:24 +0700

    complete folllowing users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,15 @@
 class User < ActiveRecord::Base
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent: :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
+
+
   
   attr_accessor :remember_token, :activation_token, :reset_token
   before_save :downcase_email
@@ -39,7 +49,8 @@ class User < ActiveRecord::Base
   end
 
   def feed
-    microposts
+    followed_ids="SELECT followed_id FROM relationships WHERE follower_id = :user_id"
+    Micropost.where("user_id IN (#{followed_ids}) OR user_id = :user_id", user_id: id)
   end
 
   # Forgets a user.
@@ -70,6 +81,21 @@ class User < ActiveRecord::Base
 
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 
 
